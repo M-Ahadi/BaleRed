6c7bdb2368db32758e8776e459b1d89f1b7f6ed3
Mojtaba Ahadi <mojtaba_2424@yahoo.com>
2018-12-08 15:35:28 +0330

    function was moved to a separate files

---
+++ models/document_message.js
@@ -0,0 +1,29 @@
+const Platform = require("balebot_plus/index");
+const FileMessage = Platform.FileMessage;
+
+module.exports = {
+
+    get_json : function (message) {
+        let content = {};
+        content.caption = message._captionText;
+        content.file_id = message._fileId;
+        content.file_hash = message._accessHash;
+        content.file_name = message._name;
+        content.file_size = message._fileSize;
+        content.thumb = message._thumb;
+        content.width = message._width;
+        content.height = message._height;
+        content.mimeType = message._mimeType;
+        return content;
+    },
+    load_json: function (msg) {
+
+        let messageObjOrFileId = msg.payload.content.file_id;
+        let file_Hash = msg.payload.content.file_hash;
+        let file_name = msg.payload.content.file_name;
+        let fileSize = msg.payload.content.file_size;
+        let mimeType = msg.payload.content.mimeType;
+        let captionText = msg.payload.content.caption;
+        return new FileMessage(messageObjOrFileId, file_Hash, file_name, fileSize, mimeType, captionText);
+    }
+};
